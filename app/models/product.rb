e7d371bd94502db41548124ca22c3a78369c78b5
Cesar Pozo <c.pozomiranda@gmail.com>
2017-02-21 16:56:59 -0300

    Scopes

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
 	belongs_to :category 
 		validates :price, presence: true
 	    before_save :set_premium_category
+	    scope :premium, -> {where(premium: true)}
+	    scope :last_n, ->(x){limit(x)}
 
 	def set_premium_category
 		 unless self.premium.present? or self.premium==true
@@ -9,4 +11,6 @@ class Product < ApplicationRecord
 		 	
 		 end
 	end
+
+
 end
